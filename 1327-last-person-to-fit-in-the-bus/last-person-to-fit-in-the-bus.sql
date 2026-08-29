8a10dc05d12846d9c5512355f3635a3f9e909cba
Ashmita Chatterjee <iashmitac12@gmail.com>
2026-08-29 16:03:51 +0530

    Time: 1917 ms (10.14%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,7 @@
-# Write your MySQL query statement below
-SELECT person_name
-FROM (
-    SELECT 
-        person_name,
-        SUM(weight) OVER (ORDER BY turn) AS total_weight
-    FROM Queue
-) q
-WHERE total_weight <= 1000
-ORDER BY total_weight DESC
+SELECT q1.person_name
+FROM Queue q1 
+JOIN Queue q2 ON q1.turn >= q2.turn
+GROUP BY q1.turn, q1.person_name
+HAVING SUM(q2.weight) <= 1000
+ORDER BY q1.turn DESC
 LIMIT 1;
